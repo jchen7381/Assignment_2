434241c30854716bac4fa8eb7d1d723afd4e0e23
Jason Chen 24079491 <73327854+jchen7381@users.noreply.github.com>
2022-10-16 22:17:09 -0400

    Add files via upload

--- test_tree.cc
+++ test_tree.cc
@@ -4,7 +4,8 @@
 
 #include "avl_tree.h"
 #include "sequence_map.h"
-
+#include <fstream>
+#include <sstream>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -18,8 +19,42 @@ namespace {
 template <typename TreeType>
 void TestTree(const string &db_filename, const string &seq_filename, TreeType &a_tree) {
 	// Code for running Part2(b)
-}
+    std::ifstream input_file;
+    input_file.open(db_filename);
+    if(input_file.fail()){
+        cout << "FILE FAILED TO OPEN" << endl;
+        exit(1);
+    }
 
+
+    std::string db_line;
+    while(getline(input_file, db_line)){
+        std::stringstream ss_db_line(db_line);
+        std::string an_enz_acro;
+        std::string a_reco_seq;
+
+        getline(ss_db_line, an_enz_acro, '/');
+        while(getline(ss_db_line, a_reco_seq)){
+            SequenceMap new_sequence_map(a_reco_seq, an_enz_acro);
+            a_tree.insert(new_sequence_map);
+        
+        }
+    }
+    input_file.close();
+    std::string userinput_sentences;
+    cout << "Enter Recognition Sequences" << endl;
+    while(cin >> userinput_sentences){
+        SequenceMap query{userinput_sentences, ""};
+        if(a_tree.contains(query)) {
+            a_tree.printValue(query);
+            
+        }
+        else{
+            std::cout << "Not Found" << std::endl;
+        }
+    }
+}
+   
 }  // namespace
 
 int
